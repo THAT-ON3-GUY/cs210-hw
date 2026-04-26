f8c5a92c3d2819690234a403bdf332330029b663
THAT-ON3-GUY <raylaser2@gmail.com>
2026-04-25 21:51:03 -0600

    Updated and added both prep 1 & 2 assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is Your Grade Percentage? ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+        string letter = "";
+        int secondDigit = grade % 10;
+
+        if (grade >= 90)
+        {
+           letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        if (secondDigit >= 7 && !(grade > 90 || grade < 60))
+        {
+            letter = letter + "+";
+        }
+        else if(secondDigit < 3 && !(grade < 60))
+        {
+            letter = letter + "-";
+        }
+
+        Console.WriteLine($"You got an {letter}!");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats! You've passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you didn't pass the class, don't give up! You can do this!");
+        }
     }
 }
