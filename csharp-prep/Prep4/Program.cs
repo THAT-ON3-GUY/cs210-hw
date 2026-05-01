8d07eaed695a8b9456c5667b49da27eda5e70345
THAT-ON3-GUY <raylaser2@gmail.com>
2026-04-30 19:56:57 -0600

    finished program 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers;
+        numbers = new List<int>();
+        string input = "";
+        int inputNum = 1;
+        int sum = 0;
+        double average = 0;
+        int largest = 0;
+
+        Console.WriteLine("Give me a series of numbers, type 0 when finished.");
+        do
+        {
+            input = Console.ReadLine();
+            inputNum = int.Parse(input);
+            numbers.Add(inputNum);
+        } while (inputNum != 0);
+
+        foreach (int digit in numbers)
+        {
+            sum = sum + digit;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        average = sum / (double)(numbers.Count-1);
+        Console.WriteLine($"The average is: {average}");
+
+        foreach (int digit in numbers)
+        {
+            if (digit > largest)
+            {
+                largest = digit;
+            }
+        }
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
