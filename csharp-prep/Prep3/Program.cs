2310ebdf986c06e1a0e03666aa289853b05c77eb
THAT-ON3-GUY <raylaser2@gmail.com>
2026-04-30 19:25:12 -0600

    Finished program 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNum = randomGenerator.Next(1, 50);
+
+        string guess = "";
+        int guessNum = 0;
+
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = Console.ReadLine();
+            guessNum = int.Parse(guess);
+
+            if (guessNum < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if(guessNum > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if(guessNum == magicNum)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else
+            {
+                Console.WriteLine("What did you do?");
+            }  
+        } while (guessNum != magicNum);
+
+
     }
 }
