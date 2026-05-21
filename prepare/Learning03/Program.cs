c521cb9deade0d72ab8cf736acb8bbd294995355
THAT-ON3-GUY <raylaser2@gmail.com>
2026-05-20 20:09:47 -0600

    Finished Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction standard = new Fraction();
+        Console.WriteLine($"{standard.GetFractionString()}");
+        Console.WriteLine($"{standard.GetDecimalValue()}");
+
+        Fraction top = new Fraction(5);
+        Console.WriteLine($"{top.GetFractionString()}");
+        Console.WriteLine($"{top.GetDecimalValue()}");
+
+        Fraction custom = new Fraction(4, 5);
+        Console.WriteLine($"{custom.GetFractionString()}");
+        Console.WriteLine($"{custom.GetDecimalValue()}");
+
+        Random random = new Random();
+        Fraction generated = new Fraction();
+        for (int i = 0; i < 20; i++)
+        {
+            int topNum = random.Next(1, 21);
+            int bottomNum = random.Next(1, 21);
+            generated.SetTop(topNum);
+            generated.SetBottom(bottomNum);
+            Console.WriteLine($"Fraction {i + 1}: string: {generated.GetFractionString()} Number: {generated.GetDecimalValue()}");
+        }
     }
 }
