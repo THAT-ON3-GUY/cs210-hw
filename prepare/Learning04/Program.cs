29e6b24fe92c16272efebd0db3bd477d9f5c5895
THAT-ON3-GUY <raylaser2@gmail.com>
2026-06-04 23:18:19 -0600

    Finished Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+            MathAssignment testMath = new MathAssignment("John Test", "Math Test", "Section 1", "Problems 1-10");
+            Console.WriteLine(testMath.GetSummary());
+            Console.WriteLine(testMath.GetHomeworkList());
+
+            WritingAssignment testWriting = new WritingAssignment("John Test", "Writing Test", "That one book");
+            Console.WriteLine(testWriting.GetSummary());
+            Console.WriteLine(testWriting.GetWritingInformation());
     }
 }
